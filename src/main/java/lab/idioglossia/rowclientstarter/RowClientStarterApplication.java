60e42cc531bc6c02db244d67975a1e1e7d324804
sepehr <sepehrgh01@gmail.com>
2020-09-06 11:56:45 +0430

    init commit

---
+++ src/main/java/lab/idioglossia/rowclientstarter/RowClientStarterApplication.java
@@ -0,0 +1,13 @@
+package lab.idioglossia.rowclientstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RowClientStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RowClientStarterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lab/idioglossia/rowclientstarter/RowClientStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package lab.idioglossia.rowclientstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RowClientStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

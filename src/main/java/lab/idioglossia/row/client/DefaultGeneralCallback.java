4ef33707e0d6a0829c805a44854fb5334461c442
sepehr-gh <sepehrgh01@gmail.com>
2020-12-13 11:20:23 +0330

    Fixed issue with DefaultGeneralCallback

--- src/main/java/lab/idioglossia/row/client/DefaultGeneralCallback.java
+++ src/main/java/lab/idioglossia/row/client/DefaultGeneralCallback.java
@@ -4,9 +4,14 @@ import lab.idioglossia.row.client.callback.GeneralCallback;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-public class DefaultGeneralCallback<E> implements GeneralCallback<E> {
+public class DefaultGeneralCallback implements GeneralCallback<String> {
     @Override
-    public <E> void onMessage(E e) {
+    public Class<String> getClassOfCallback() {
+        return String.class;
+    }
+
+    @Override
+    public void onMessage(String message) {
         log.warn("Implement general callback to get messages published from server into no specific channel");
     }
 }
